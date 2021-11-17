50bd666427bd4519578fc110fd80ed92191d181e
kuril227 <kirill.otdelnov@gmail.com>
2021-11-17 23:41:43 +0300

    end

--- main.py
+++ main.py
@@ -4,10 +4,6 @@ import SimplexMethod
 if __name__ == '__main__':
     A, c, b = from_file.get_data(open("input.txt", "r"))
     res = SimplexMethod.SimplexMethod(A, c, b)
-    for i in range(len(res.simplex_table)):
-        print(res.simplex_table[i])
+    res.__repr__()
     print()
     res.iteration()
-    # for i in range(len(res.simplex_table)):
-    #     print(res.simplex_table[i])
-    # print(res.__repr__())
