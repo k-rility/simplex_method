c0c91f7b487f876e80e09e3c8652efe0a8806cd2
kuril227 <kirill.otdelnov@gmail.com>
2021-12-02 11:55:12 +0300

    last decided

--- SimplexMethod.py
+++ SimplexMethod.py
@@ -1,74 +1,181 @@
 import copy
-from tabulate import tabulate
+import numpy as np
+from prettytable import PrettyTable
 
 
 class SimplexMethod:
 
-    def __init__(self, A, c, b):
-        self.simplex_table = A
-        for i in range(len(self.simplex_table)):
-            self.simplex_table[i].append(b[i])
-        self.simplex_table.append([-i for i in c])
-        self.simplex_table[len(self.simplex_table) - 1].append(0)
+    # в функции __init__ создаем матрицу из предоставленных данных, которая будет символизировать симплекс таблицу и с
+    # через нее же будем находить опорную симплекс таблицу и оптимальные решения
 
-        self.old_simplex_table = copy.deepcopy(self.simplex_table)
-        self.ind_row = 0
-        self.ind_col = 0
+    def __init__(self, A, c, b, MinMax):
+        self.MinMax = MinMax
 
-    def perm_col(self):
-        min_c = self.simplex_table[len(self.simplex_table) - 1][0]
-        for i in range(len(self.simplex_table) - 1):
-            if self.simplex_table[len(self.simplex_table) - 1][i] <= min_c:
-                min_c = self.simplex_table[len(self.simplex_table) - 1][i]
-                self.ind_col = i
+        # создаем временную матрицу, в которую добавим все данные
+        temp = A
+        for i in range(len(temp)):
+            temp[i].append(b[i])
+        temp.append(c)
+        temp[len(temp) - 1].append(0)
 
-    def perm_row(self):
-        min_r = self.simplex_table[0][len(self.simplex_table[0]) - 1] / self.simplex_table[0][self.ind_col]
-        for i in range(len(self.simplex_table) - 1):
-            flag = self.simplex_table[i][self.ind_col] != 0
-            if self.simplex_table[i][
-                self.ind_col] > 0 and flag and self.simplex_table[i][len(self.simplex_table) - 1] / \
-                    self.simplex_table[i][
-                        self.ind_col] <= min_r:
-                min_r = self.simplex_table[i][len(self.simplex_table) - 1] / self.simplex_table[i][self.ind_col]
-                self.ind_row = i
+        # копируем ту же матрицу как матрицу из модуля numpy
+
+        self.SimplexTable = np.matrix(temp)
+
+        # запоминаем количество строк и столбов в этой матрице, чтобы избежать ищлишне большого кода
+
+        self.RowNum, self.ColNum = self.SimplexTable.shape
+        # self.SimplexTable[self.RowNum - 1] *= -1
+
+        # это матрица, которая будет соответствовать матрице не подвергающаяся изменениям через Жордановы преобразования
+
+        self.OldSimplexTable = copy.deepcopy(self.SimplexTable)
+
+        # индексы разрешающего элемента в матрице
+
+        self.IdxRow = 0
+        self.IdxCol = 0
+
+        # поля для более понятного представления симплекс таблицы
+
+        self.Headers = [''] + [f'x{i}' for i in range(1, self.ColNum)] + ['C']
+        self.BaseCol = [[f'u{i}'] for i in range(1, self.RowNum)] + [['F']]
+
+    # функция ниже возвращает значение разрешающего элемента обращаясь к нему через определенные ранее индексы
 
     def get_pivot(self):
-        return self.simplex_table[self.ind_row][self.ind_col]
+        return self.SimplexTable[self.IdxRow, self.IdxCol]
+
+    # преобразовываем элементы матрице через функцию ниже
 
     def jordan_transform(self):
-        pivot = self.get_pivot()
-        for i in range(len(self.simplex_table[0])):
-            self.simplex_table[self.ind_row][i] /= pivot
-        for i in range(len(self.simplex_table)):
-            self.simplex_table[i][self.ind_col] /= -pivot
-        self.simplex_table[self.ind_row][self.ind_col] = -self.simplex_table[self.ind_row][self.ind_col]
 
-        for i in range(len(self.simplex_table)):
-            for j in range(len(self.simplex_table[0])):
-                if i == self.ind_row or j == self.ind_col:
+        # находим значение разрешающего элемента
+
+        pivot = self.get_pivot()
+
+        # делим разрешающую строку на разрешающий элемент
+
+        self.SimplexTable[self.IdxRow] /= pivot
+
+        # делим разрешающий столбец на разрешающий элемент, взятый со знаком минус
+
+        self.SimplexTable[:, self.IdxCol] /= -pivot
+
+        # также домножаем разрешающий элемент, находящийся в матрице на минус единицу, чтобы в связи с преобразованиями
+        # произведенными выше, он не изменил свой знак
+
+        self.SimplexTable[self.IdxRow, self.IdxCol] *= -1
+
+        # преобразовываем элементы матрицы, которые не входят в разрешающий столбец и разрешающую строку
+
+        for i in range(self.RowNum):
+            for j in range(self.ColNum):
+
+                # если элемент состоит в разрещающей строке или столбце, то продолжаем поиск тех элементов, которые туда
+                # не входят
+
+                if i == self.IdxRow or j == self.IdxCol:
                     continue
                 else:
-                    self.simplex_table[i][j] = (pivot * self.simplex_table[i][j] -
-                                                self.old_simplex_table[self.ind_row][j] *
-                                                self.old_simplex_table[i][self.ind_col]) / pivot
 
-        self.old_simplex_table = copy.deepcopy(self.simplex_table)
+                    # если нашли такие элементы, то преобразовываем их по формуле
 
-    def iteration(self):
-        while min(self.simplex_table[len(self.simplex_table) - 1]) < 0:
-            self.perm_col()
-            self.perm_row()
+                    self.SimplexTable[i, j] = (pivot * self.SimplexTable[i, j] -
+                                               self.OldSimplexTable[self.IdxRow, j] *
+                                               self.OldSimplexTable[i, self.IdxCol]) / pivot
+
+        self.OldSimplexTable = copy.deepcopy(self.SimplexTable)
+
+    # функция для проверки неотрицательности свободных членов
+
+    def check_free_members(self):
+        flag = True
+        for i in range(self.RowNum - 1):
+            if self.SimplexTable[i, self.ColNum - 1] < 0:
+                flag = False
+                break
+        return flag
+
+    # функция для поиска разрешающего столбца
+
+    def ref_perm_col(self):
+        if self.SimplexTable[0:self.RowNum - 1, self.ColNum - 1].min() < 0:
+            self.IdxCol = self.SimplexTable[0:self.RowNum - 1, self.ColNum - 1].argmin()
+
+    def ref_perm_row(self):
+        MinRowArg = None
+
+        for i in range(self.RowNum - 1):
+            flag = self.SimplexTable[i, self.IdxCol] != 0
+            if MinRowArg == None and flag and self.SimplexTable[i, self.ColNum - 1] / self.SimplexTable[
+                i, self.IdxCol] > 0:
+                MinRowArg = self.SimplexTable[i, self.ColNum - 1] / self.SimplexTable[i, self.IdxCol]
+                self.IdxRow = i
+            elif flag and self.SimplexTable[i, self.ColNum - 1] / self.SimplexTable[i, self.IdxCol] > 0 and \
+                    self.SimplexTable[i, self.ColNum - 1] / self.SimplexTable[i, self.IdxCol] <= MinRowArg:
+                MinRowArg = self.SimplexTable[i, self.ColNum - 1] / self.SimplexTable[i, self.IdxCol]
+                self.IdxRow = i
+
+    # функция для поиска опорного решения, совмещающая в себе все функции описанные выше
+
+    def ref_solution(self):
+        iteration = 1
+        while not self.check_free_members():
+            self.ref_perm_col()
+            self.ref_perm_row()
             self.jordan_transform()
             print()
-            self.__repr__()
+            print(self.SimplexTable)
+            print(f'iteration: {iteration}')
+            print("permission col: ", self.IdxCol)
+            print("permission row: ", self.IdxRow)
+            iteration += 1
 
-    def __repr__(self):
+        # функция для поиска оптимального решения, которая вызывается при нахождении оптимального решения
 
-        column_list = ['    '] + [f'    x{i}    ' for i in range(1, len(self.simplex_table))] + ['  C   ']
-        value_list = [[f'x{i}'] for i in range(1, 4)] + [['F']]
-        for i in range(len(value_list)):
-            for j in range(len(value_list)):
-                value_list[i].append(self.simplex_table[i][j])
+        self.optimal()
 
-        return print(tabulate(value_list, column_list, tablefmt='grid', stralign='center'))
+    def check_obj_func(self):
+        flag = True
+        for i in range(self.ColNum - 1):
+            if self.SimplexTable[self.RowNum - 1, i] > 0:
+                flag = False
+                break
+        return flag
+
+    def perm_col(self):
+        if self.SimplexTable[self.RowNum - 1, 0:self.ColNum - 1].max() > 0:
+            self.IdxCol = self.SimplexTable[self.RowNum - 1, 0:self.ColNum - 1].argmax()
+
+    # функция для поиска оптимального решения
+
+    def optimal(self):
+        iteration = 1
+        self.print_table()
+
+        while not self.check_obj_func():
+            self.perm_col()
+            self.ref_perm_row()
+            self.jordan_transform()
+            print()
+
+            temp = self.BaseCol[self.IdxRow]
+            self.BaseCol[self.IdxRow] = [self.Headers[self.IdxCol + 1]]
+            self.Headers[self.IdxCol + 1] = temp[0]
+            self.print_table()
+
+            print(f'iteration: {iteration}')
+            print("permission col: ", self.IdxCol)
+            print("permission row: ", self.IdxRow)
+            iteration += 1
+
+    def print_table(self):
+        ReprTable = PrettyTable()
+        ReprTable.field_names = self.Headers
+        for i in range(self.RowNum):
+            if i == self.RowNum - 1:
+                ReprTable.add_row(self.BaseCol[len(self.BaseCol) - 1] + np.array(self.SimplexTable)[i].tolist())
+            else:
+                ReprTable.add_row(self.BaseCol[i] + np.array(self.SimplexTable)[i].tolist())
+        return print(ReprTable)
--- main.py
+++ main.py
@@ -1,9 +1,20 @@
-import from_file
+import from_input
 import SimplexMethod
+import copy
 
 if __name__ == '__main__':
-    A, c, b = from_file.get_data(open("input.txt", "r"))
-    res = SimplexMethod.SimplexMethod(A, c, b)
-    res.__repr__()
-    print()
-    res.iteration()
+    A, c, b, MinMax = from_input.get_data(open("input.txt", "r"))
+    res = SimplexMethod.SimplexMethod(A, c, b, MinMax)
+    # print(res.ref_perm_col())
+    # print(res.ref_perm_row())
+    res.ref_solution()
+    # print(res.SimplexTable)
+    # print(res.ref_perm_row())
+    # print(res.SimplexTable[:, res.ColNum - 1])
+    # print()
+    # a = int(min(res.SimplexTable[:, res.ColNum - 1]))
+    # print(a)
+
+    # res.__repr__()
+    # print()
+    # res.iteration()
